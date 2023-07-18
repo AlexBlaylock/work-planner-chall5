fa360710a3b7387f37569fcbe950aeb3265ffa3f
Alex Blaylock <129992589+AlexBlaylock@users.noreply.github.com>
2023-07-18 18:22:48 -0500

    added time blocks, working on function for time blocks

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,8 @@
+function timeApplication() {
+  const currentTime = dayjs().format("Hr");
+
+  $()
+}
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
