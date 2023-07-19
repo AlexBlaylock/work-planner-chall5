c3b99becb03e6719424a14d7bd1483c774435285
Alex Blaylock <129992589+AlexBlaylock@users.noreply.github.com>
2023-07-19 05:03:47 -0500

    working on function loadSavedEvents

--- assets/script.js
+++ assets/script.js
@@ -9,10 +9,17 @@ function dayTimer() {
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-$(function () {
+// loads saved data
+function loadSavedEvents() {
+  timeSlots.forEach((element) => {
+    var textBox = localStorage.getItem(parseInt(element.hourSave))
+    // saying if 
+    if (textBox) {
+      element.text.val(textBox)
+    }
+  })
+}
  
-});
-
 // save btn event
 
 $(".saveBtn").click(function(event) {
@@ -21,11 +28,11 @@ $(".saveBtn").click(function(event) {
   var elementSaver = $(this).siblings("textarea")
   var hourSave = elementSaver.attr("id");
   console.log(hourSave)
-  var textSave = elementSaver.val();
-  console.log(textSave)
+  var textBox = elementSaver.val();
+  console.log(textBox)
   // local storage save
-  if (hourSave && text !== "") {
-    console.log(hourSave, textSave);
+  if (hourSave && textBox !== "") {
+    console.log(hourSave, textBox);
     localStorage.setItem(hourSave, textSave)
   }
 })
