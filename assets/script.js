6d09678e40edcf454b16cae056252ff9ecc5067a
Alex Blaylock <129992589+AlexBlaylock@users.noreply.github.com>
2023-07-19 03:47:57 -0500

    added local storage save/save btn (i thinks lol)

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,10 @@
 var todayDisplay = $("#todayTime");
 
 function dayTimer() {
-
-var todayTime = dayjs().format("dddd, MMMM D YYYY, h:mm a");
-todayDisplay.text(todayTime);
+  var todayTime = dayjs().format("dddd, MMMM D YYYY, h:mm a");
+  todayDisplay.text(todayTime);
 // setting interval to update every minute
-setInterval(dayTimer, 60000);
-
+  setInterval(dayTimer, 60000);
 };
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
@@ -34,5 +32,18 @@ $(function () {
 
 // save btn event
 
+$(".saveBtn").click(function(event) {
+  event.preventDefault();
+
+  var elementSaver = $(this).parent().prop("id")
+  var hourSave = elementSaver.attr("id");
+  var textSave = elementSaver.val();
+
+  // local storage save
+  if (hourSave && text !== "") {
+    console.log(hourSave, textSave);
+    localStorage.setItem(hourSave, textSave)
+  }
+})
 // loads timer on page start
 dayTimer();
