7680be07a74a6ed890e57bbd8f8b1903326dc47b
Alex Blaylock <129992589+AlexBlaylock@users.noreply.github.com>
2023-07-19 03:51:10 -0500

    changed attr on line 22 to prop

--- assets/script.js
+++ assets/script.js
@@ -10,24 +10,7 @@ function dayTimer() {
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+ 
 });
 
 // save btn event
@@ -36,7 +19,7 @@ $(".saveBtn").click(function(event) {
   event.preventDefault();
 
   var elementSaver = $(this).parent().prop("id")
-  var hourSave = elementSaver.attr("id");
+  var hourSave = elementSaver.prop("id");
   var textSave = elementSaver.val();
 
   // local storage save
